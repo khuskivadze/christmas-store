5da911f6c6b2dcf0bcc5331e809efd5d554d1952
khuskivadze <tamarkhuskivadze94@gmail.com>
2023-12-04 01:03:10 +0400

    add products

--- app/shop/Products/Products.tsx
+++ app/shop/Products/Products.tsx
@@ -3,21 +3,31 @@ import styles from './Products.module.css'
 
 
 
+ type Props = {
+    bottomText : string,
+    title :string,
+    price: string,
+    button:string,
+    img:string,
+    line?: string
+
+ }
 
 
-
-export default () => {
+export default (props: Props) => {
     return (
         <div className= {styles.card}>
             <div className= {styles.image}>
-                <span className= {styles.bottomText}>Queek view</span>
+                <img src={props.img} alt="" />
+                <span className= {styles.bottomText}> {props.bottomText} </span>
+                
             </div>
             <div className= {styles.info}>
-                <p className= {styles.title}>I'm a product</p>
+                <p className= {styles.title}> {props.title}</p>
                 <span className= {styles.line}></span>
-                <span className= {styles.price}>$400.00</span>
+                <span className= {styles.price}>{props.price}</span>
             </div>
-            <button className= {styles.button}>Add to cart</button>
+            <button className= {styles.button}> {props.button}</button>
 
         </div>
     )
--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -22,24 +22,30 @@ export default function shop () {
                      <h1>All Products</h1>
                    </div>
                    <div className= {styles.cardContainer}>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
-                        <Products/>
+                        <Products img='/images/84770f_3d61665c560442849e3ac2f6f7675427~mv2_d_1500_1500_s_2.webp' bottomText ='Queek view' title ='I am product' price='$400.00' button='Add to Cart'/>
+                        <Products img='/images/card2.webp'  bottomText ='Queek view' title ='I am product' price='$400.00' button='Add to Cart'/>
+                        <Products img='/images/card3.webp'  bottomText ='Queek view' title ='I am product' price='$700.00' button='Add to Cart'/>
+                        <Products img='/images/card4.webp'  bottomText ='Queek view' title ='I am product' price='$200.00' button='Add to Cart'/>
+                        <Products img='/images/card5.webp'  bottomText ='Queek view' title ='I am product' price='$100.00' button='Add to Cart'/>
+                        <Products img='/images/card6.webp'  bottomText ='Queek view' title ='I am product' price='$200.00' button='Add to Cart'/>
+                        <Products img='/images/card7.webp'  bottomText ='Queek view' title ='I am product' price='$400.00' button='Add to Cart'/>
+                        <Products img='/images/card8.webp'  bottomText ='Queek view' title ='I am product' price='$600.00' button='Add to Cart'/>
+                        <Products img='/images/card9.webp'  bottomText ='Queek view' title ='I am product' price='$100.00' button='Add to Cart'/>
+                        <Products img='/images/card10.webp' bottomText ='Queek view' title ='I am product' price='$100.00' button='Add to Cart'/>
+                        <Products img='/images/card11.webp' bottomText ='Queek view' title ='I am product' price='$200.00' button='Add to Cart'/>
+                        <Products img='/images/card12.webp' bottomText ='Queek view' title ='I am product' price='$450.00' button='Add to Cart'/>
+                        <Products img='/images/card13.webp' bottomText ='Queek view' title ='I am product' price='$300.00' button='Add to Cart'/>
+                        <Products img='/images/card14.webp' bottomText ='Queek view' title ='I am product' price='$120.00' button='Add to Cart'/>
+                        <Products img='/images/card15.webp' bottomText ='Queek view' title ='I am product' price='$320.00' button='Add to Cart'/>
+                        <Products img='/images/card16.webp' bottomText ='Queek view' title ='I am product' price='$15.00' button='Add to Cart'/>
+                        <Products img='/images/card17.webp' bottomText ='Queek view' title ='I am product' price='$400.00' button='Add to Cart'/>
+                        <Products img='/images/card18.webp' bottomText ='Queek view' title ='I am product' price='$400.00' button='Add to Cart'/>
+
+
+                         
+
+
+                        
                     </div>
                    
 
